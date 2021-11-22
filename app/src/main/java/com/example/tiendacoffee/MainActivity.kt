4bb6e484eb48d5b1178468eea27b4cfb0482e437
Jhonatan <jdasprilla14@misena.edu.co>
2021-11-21 22:10:25 -0500

    Home desarrollado

--- app/src/main/java/com/example/tiendacoffee/MainActivity.kt
+++ app/src/main/java/com/example/tiendacoffee/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.tiendacoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
         //setContentView(R.layout.bienvenida)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar()?.hide(); //hide the title bar
+        setContentView(R.layout.home_screen)
     }
 }
