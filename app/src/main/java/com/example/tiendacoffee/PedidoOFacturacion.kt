f06bb20704f6a201aa1e2cbc4b955945a9338672
jirafelipeguantiva <pfguantivac@unal.edu.co>
2021-11-27 21:59:33 -0500

    vitas_pedidos_y_comentarios

---
+++ app/src/main/java/com/example/tiendacoffee/PedidoOFacturacion.kt
@@ -0,0 +1,11 @@
+package com.example.tiendacoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PedidoOFacturacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pedido_ofacturacion)
+    }
+}
---
+++ app/src/main/java/com/example/tiendacoffee/comentarios_y_pedidos.kt
@@ -0,0 +1,11 @@
+package com.example.tiendacoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class comentarios_y_pedidos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.comentarios_y_pedidos)
+    }
+}
